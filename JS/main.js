04a5fdf22898e36d206b07acb034ca42ed7f6060
Jwmffreitas <jwmf.freitas@gmail.com>
2020-06-22 11:54:54 -0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,44 @@
-alert("meu primeiro js")
+var d = new Date();
+alert(d.getMinutes());
+
+/* var count;
+for(count=0; count <=5; count++){
+    alert(count);
+} */
+
+/* var count = 0;
+while (count <= 5) {
+    console.log(count);
+    alert(count);
+    count++;
+} */
+
+/* var idade = prompt("Qual a sua idade?");
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+} */
+
+/* var frutas = [{nome: "maçã", cor:"vermelha"}, {nome:"uva" , cor:"roxa"}]
+console.log(frutas);
+alert(frutas[0].nome) */
+
+/* var fruta = {nome: "maçã", cor:"vermelha"}
+console.log(fruta.nome);
+console.log(fruta.cor); */
+
+/* var lista = ["Maçã", "Pera", "Uva"];
+lista.push("Laranja");
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" - ")); */
+
+/* var nome = "João Vitor";
+var idade = 17;
+var idade2 = 8
+var frase = "Japão é incrível"
+alert(nome + " tem" + idade + " anos");
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil")); */

ce801c7c1de6fcc6a4e76b037c0b482fa07bc24e
Jwmffreitas <jwmf.freitas@gmail.com>
2020-07-06 16:24:56 -0300

    git commit

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+  function troca(i) {
+      if(i == 1) {
+          document.getElementById('mag').src = "img/Mag.png"
+      }else{
+          document.getElementById('mag').src= "img/Magneto.png"
+      }
+  }

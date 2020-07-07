76cb17ed0fa65fbd469726235391e6e6f2f9f1ad
Jwmffreitas <jwmf.freitas@gmail.com>
2020-07-07 10:54:13 -0300

    git commit

--- JS/main.js
+++ JS/main.js
@@ -5,3 +5,18 @@
           document.getElementById('mag').src= "img/Magneto.png"
       }
   }
+
+  function mutante() {
+      var fav = document.getElementById('favorit')
+      var res = document.getElementById('res')
+      var conj = ['Wolverine', 'Cyclops', 'Jean Grey', 'Rogue', 'Iceman']
+      var conjimg = []
+      var fav1 = Number(fav.value) - 1
+      let final = conj[fav1]
+
+      if(fav.value == '' || Number(fav.value > 5)) {
+        res.innerHTML = `Please, digit a number between 1 and 3`
+      }else {
+        res.innerHTML = `Your favorite mutant is ${final}`
+      }
+  }

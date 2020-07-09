bfdb08be0862efeff4c7b333617b5b77e856df88
Jwmffreitas <jwmf.freitas@gmail.com>
2020-07-09 16:37:30 -0300

    git commit

--- JS/main.js
+++ JS/main.js
@@ -10,13 +10,15 @@
       var fav = document.getElementById('favorit')
       var res = document.getElementById('res')
       var conj = ['Wolverine', 'Cyclops', 'Jean Grey', 'Rogue', 'Iceman']
-      var conjimg = []
+      var conjimg = ['img/Wolv.jpg', '', '', '', '']
       var fav1 = Number(fav.value) - 1
       let final = conj[fav1]
+      let finalimg = conjimg[fav1]
 
       if(fav.value == '' || Number(fav.value > 5)) {
         res.innerHTML = `Please, digit a number between 1 and 3`
       }else {
-        res.innerHTML = `Your favorite mutant is ${final}`
+        res.innerHTML = `Your favorite mutant is ${final} </br>`
+        res.innerHTML += `<img class="wolv" src="${finalimg}" alt=""></img>`
       }
   }

212a88771e3caf42d52f5733f410b1e97d27ac34
Jwmffreitas <jwmf.freitas@gmail.com>
2020-07-10 12:55:18 -0300

    git commit

--- JS/main.js
+++ JS/main.js
@@ -11,6 +11,7 @@
       var res = document.getElementById('res')
       var conj = ['Wolverine', 'Cyclops', 'Jean Grey', 'Rogue', 'Iceman']
       var conjimg = ['img/Wolv.jpg', 'img/cyclops.jpg', 'img/jean.jpg', 'img/rogue.jpg', 'img/iceman.jpg']
+      var habw = ['Garras de adamantium', 'Fator de cura', 'Sentidos aguçados', 'Reflexos sobre-humanos', 'Visão Noturna']
       var fav1 = Number(fav.value) - 1
       let final = conj[fav1]
       let finalimg = conjimg[fav1]
@@ -19,6 +20,9 @@
         res.innerHTML = `Please, digit a number between 1 and 5`
       }else {
         res.innerHTML = `Your favorite mutant is ${final} </br>`
-        res.innerHTML += `<img class="wolv" src="${finalimg}" alt=""></img>`
+        res.innerHTML += `<img class="wolv" src="${finalimg}" alt=""></img> <h1>Habilidades:</h1>`
+      }
+      if(fav.value == '1') {
+        res.innerHTML += `${habw[0]} </br> ${habw[1]} </br> ${habw[2]} </br> ${habw[3]} </br> ${habw[4]}`
       }
   }

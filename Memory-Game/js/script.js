3579cb49d47e6b675c51174ff350315dd7fa82f0
Jwmffreitas <jwmf.freitas@gmail.com>
2020-10-21 13:35:15 -0300

    verificando se as cartas são iguais

--- Memory-Game/js/script.js
+++ Memory-Game/js/script.js
@@ -1,5 +1,39 @@
 var Card
+var Abriu = 0
+var Acertos = 0
+var id1
+var id2 
+var c1
+var c2
 
 function virarCard(Card) {
-    console.log($(Card).attr('name'))
+    if(Abriu == 0) {
+        c1 = $(Card).attr('name')
+        $(Card).css('transform', 'rotateY(180deg)')
+        Abriu = Abriu + 1
+        id1 = Card
+        console.log(Card)   
+    }else if(Abriu == 1) {
+        c2 = $(Card).attr('name')
+        $(Card).css('transform', 'rotateY(180deg)')
+        Abriu = Abriu + 1
+        id2 = Card
+        console.log(Card)
+        console.log(`${c1} e ${c2}`)
+        Abriu = 0
+        if(c1 == c2) {
+            Acertos = Acertos + 1
+            c1 = ''
+            c2 = ''
+        }else {
+            setTimeout(() => {
+                $(id1).css('transform', 'rotateY(0deg)')
+                $(id2).css('transform', 'rotateY(0deg)')
+                c1 = ''
+                c2 = ''
+                id1 = ''
+                id2 = ''
+            }, 1500)
+        }
+    }
 }
